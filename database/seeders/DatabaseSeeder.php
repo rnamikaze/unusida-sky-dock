fb787fcc140ce7198a737fe3f4e49881d0d00f64
Ryuzen Namikaze <rnamikaze@proton.me>
2025-09-12 15:56:50 +0700

    Added Traffic Status Seeder and second admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\UserTrafficStatus;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -16,9 +18,22 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\Admin::factory()->create([
-            'name' => 'Fitzgerald Kennedy',
-            'email' => 'rnamikaze@proton.me',
-            'password' => Hash::make('rnamikaze@proton.me'),
+            'name' => 'Dwight Eisenhower',
+            'email' => 'rnamikaze@hotmail.com',
+            'password' => Hash::make('5b5e560b6167305469d52eaa7058fcbe37fb0b381f4e02f9672fbf46ff430925'),
         ]);
+
+        $allUser = User::all();
+
+        foreach ($allUser as $user) {
+            if (UserTrafficStatus::where('user_id', $user->id)->first()) {
+                continue;
+            }
+
+            UserTrafficStatus::create([
+                "user_id" => $user->id,
+                "allow" => true
+            ]);
+        }
     }
 }
